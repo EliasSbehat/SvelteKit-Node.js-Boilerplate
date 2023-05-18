4ebbf36af278e0e236b4159b55e77fc364898933
elias <kompanipejper@gmail.com>
2023-05-19 04:51:29 +0900

    add signup UI

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Signup</title>
+	<meta name="description" content="Signup this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Signup this app</h1>
+
+	<div class="login-container">
+		<form action="">
+			<input type="text" placeholder="Name" /> <br /><br />
+			<input type="text" placeholder="Email" /> <br /><br />
+			<input type="password" placeholder="Password" /><br /><br />
+			<input type="password" placeholder="Confirm Password" />
+			<button>signup</button>
+		</form>
+	</div>
+</div>
